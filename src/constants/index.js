26058ca29618096877fcbc945ad222a58bb2d1a9
Kayl Eubanks <kayleubanks@gmail.com>
2018-07-20 11:52:20 -0700

    Adding initial state values for development

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,12 +5,58 @@ export const initialState = {
     answerPoints: []
   },
   game data: {
-    answerArtists: ['string', 'string', ..., 'string'],
-    songData: [{
+    answerArtists: ['00FQb4jTyendYWaN8pK0wa', '5a2EaR3hamoenG9rDuVn8j', '0O0lrN34wrcuBenkqlEDZe', '06HL4z0CvFAxyc27GXpf02', '08GQAI4eElDnROBrJRGE0X', '1dfeR4HaWDbWqFHLkxsg1d', '3WrFJ7ztbogyGnTHbHJFl2', '7guDJrEfX3qb6FEbdPA5qi', '6vWDO969PvNqNYHIOW5v0m', '0oSGxfWSnnOXhD2fKuz2Gy'],
+    songData: [
+      {
         trackName: 'string',
-        artistName: 'string',
+        artistName: 'Lana Del Rey',
         trackAudio: 'string'
-      }, {...}, ..., {...}],
+      },
+      {
+        trackName: 'When Doves Cry',
+        artistName: 'Prince',
+        trackAudio: 'https://p.scdn.co/mp3-preview/5fc1bc59898ea8d0d23cf88810828d014020ef5d?cid=e0798695220f4b1296dc84f83a2c201c'
+      },
+      {
+        trackName: 'The Ghost In You',
+        artistName: 'The Psychedelic Furs',
+        trackAudio: 'https://p.scdn.co/mp3-preview/cb0c02a872e3e1329bcf998575392ad6fff3a72b?cid=e0798695220f4b1296dc84f83a2c201c'
+      },
+      {
+        trackName: 'string',
+        artistName: 'Taylor Swift',
+        trackAudio: 'string'
+      },
+      {
+        trackName: 'Dreams',
+        artistName: 'Fleetwood Mac',
+        trackAudio: 'https://p.scdn.co/mp3-preview/40d1f6d82f16b05b62aae47e8e130de04b407fc8?cid=e0798695220f4b1296dc84f83a2c201c'
+      },
+      {
+        trackName: 'string',
+        artistName: 'Queen',
+        trackAudio: 'string'
+      },
+      {
+        trackName: 'string',
+        artistName: 'The Beatles',
+        trackAudio: 'string'
+      },
+      {
+        trackName: 'string',
+        artistName: 'Stevie Wonder',
+        trackAudio: 'string'
+      },
+      {
+        trackName: 'Halo',
+        artistName: 'Beyoncé',
+        trackAudio: 'https://p.scdn.co/mp3-preview/3c97985f3736fab6d4abcd2067f346a9b30955fa?cid=e0798695220f4b1296dc84f83a2c201c'
+      },
+      {
+        trackName: 'Heroes',
+        artistName: 'David Bowie',
+        trackAudio: 'https://p.scdn.co/mp3-preview/92caa5df4a00974b3065c2db0bb284dae528f62a?cid=e0798695220f4b1296dc84f83a2c201c'
+      }],
     wrongArtistNames: [['string', 'string', ..., 'string'], [...], ..., [...]]
   },
   user authentication: {
