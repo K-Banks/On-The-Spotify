293d5a8f6b009dc7cb6c82003b4c8aeb40b7d99a
Kayl Eubanks <kayleubanks@gmail.com>
2018-07-20 15:27:36 -0700

    write function that randomizes all 4 potential answers

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -1,23 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Game(props) {
-    let randomizedAnswers = [];
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.randomizedAnswersArray=[];
+    this.randomizeAnswers = this.randomizeAnswers.bind(this);
+  }
 
-    function logState() {
-      console.log(props.state);
-    };
+  didComponentMount() {
+    this.randomizeAnswers();
+  }
 
-  return(
-    <div>
-      <h1>THIS IS WORKING</h1>
-      <button onClick={() => {logState();}}>Press button to print state to console.</button>
-      <p>{props.state.gameData.songData[props.state.currentRound].artistName}</p>
-      {props.state.gameData.wrongArtistNames[props.state.currentRound].map((artist, key) =>
-        <p key={key}>{artist}</p>
-      )}
-    </div>
-  );
+  randomizeAnswers() {
+    let answer1 = this.props.state.gameData.songData[this.props.state.currentRound].artistName;
+    let answer2 = this.props.state.gameData.wrongArtistNames[this.props.state.currentRound][0];
+    let answer3 = this.props.state.gameData.wrongArtistNames[this.props.state.currentRound][1];
+    let answer4 = this.props.state.gameData.wrongArtistNames[this.props.state.currentRound][2];
+    const answerArray = [answer1, answer2, answer3, answer4];
+    // DEBUG:
+    while (this.randomizedAnswersArray.length < 4) {
+      let selection = Math.floor(Math.random() * 4);
+      if (this.randomizedAnswersArray.includes(answerArray[selection]) === false) {
+        this.randomizedAnswersArray.push(answerArray[selection]);
+      }
+    }
+    console.log(this.randomizedAnswersArray);
+  };
+
+  logState() {
+    console.log(this.props.state);
+  };
+
+  render(){
+    return(
+      <div>
+        <h1>THIS IS WORKING</h1>
+        <button onClick={() => {this.logState();}}>Press button to print state to console.</button>
+        {this.randomizedAnswersArray.map((artist, key) =>
+          <p key={key}>{artist}</p>
+        )}
+      </div>
+    );
+  }
 }
 
   Game.propTypes = {
