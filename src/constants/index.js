0f48dcc1938fe41d41571255a9c6f6bf3bcae6ca
Kayl Eubanks <kayleubanks@gmail.com>
2018-07-20 12:17:04 -0700

    Finish hardcoding data into initial state for testing. lots of manual api calls

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,12 +5,12 @@ export const initialState = {
     answerPoints: []
   },
   game data: {
-    answerArtists: ['00FQb4jTyendYWaN8pK0wa', '5a2EaR3hamoenG9rDuVn8j', '0O0lrN34wrcuBenkqlEDZe', '06HL4z0CvFAxyc27GXpf02', '08GQAI4eElDnROBrJRGE0X', '1dfeR4HaWDbWqFHLkxsg1d', '3WrFJ7ztbogyGnTHbHJFl2', '7guDJrEfX3qb6FEbdPA5qi', '6vWDO969PvNqNYHIOW5v0m', '0oSGxfWSnnOXhD2fKuz2Gy'],
+    answerArtists: ['7Ln80lUS6He07XvHI8qqHH', '5a2EaR3hamoenG9rDuVn8j', '0O0lrN34wrcuBenkqlEDZe', '06HL4z0CvFAxyc27GXpf02', '08GQAI4eElDnROBrJRGE0X', '1dfeR4HaWDbWqFHLkxsg1d', '3WrFJ7ztbogyGnTHbHJFl2', '7guDJrEfX3qb6FEbdPA5qi', '6vWDO969PvNqNYHIOW5v0m', '0oSGxfWSnnOXhD2fKuz2Gy'],
     songData: [
       {
-        trackName: 'string',
-        artistName: 'Lana Del Rey',
-        trackAudio: 'string'
+        trackName: 'Do I Wanna Know?',
+        artistName: 'Arctic Monkeys',
+        trackAudio: 'https://p.scdn.co/mp3-preview/73e00a0a59c897b16d0fe30df43f7aeb2997079d?cid=e0798695220f4b1296dc84f83a2c201c'
       },
       {
         trackName: 'When Doves Cry',
@@ -23,9 +23,9 @@ export const initialState = {
         trackAudio: 'https://p.scdn.co/mp3-preview/cb0c02a872e3e1329bcf998575392ad6fff3a72b?cid=e0798695220f4b1296dc84f83a2c201c'
       },
       {
-        trackName: 'string',
+        trackName: 'Shake It Off',
         artistName: 'Taylor Swift',
-        trackAudio: 'string'
+        trackAudio: 'https://p.scdn.co/mp3-preview/7c08a9f4d871b5cb758fb7dd95abc8b06c9ed423?cid=e0798695220f4b1296dc84f83a2c201c'
       },
       {
         trackName: 'Dreams',
@@ -33,19 +33,19 @@ export const initialState = {
         trackAudio: 'https://p.scdn.co/mp3-preview/40d1f6d82f16b05b62aae47e8e130de04b407fc8?cid=e0798695220f4b1296dc84f83a2c201c'
       },
       {
-        trackName: 'string',
+        trackName: 'Crazy Little Thing Called Love',
         artistName: 'Queen',
-        trackAudio: 'string'
+        trackAudio: 'https://p.scdn.co/mp3-preview/ef98de2b63766a0e86ce68272037971f673ae744?cid=e0798695220f4b1296dc84f83a2c201c'
       },
       {
-        trackName: 'string',
+        trackName: 'Blackbird',
         artistName: 'The Beatles',
-        trackAudio: 'string'
+        trackAudio: 'https://p.scdn.co/mp3-preview/9cd5790f74f29046953ef511c3737a1121785b73?cid=e0798695220f4b1296dc84f83a2c201c'
       },
       {
-        trackName: 'string',
+        trackName: 'Isn\'t She Lovely',
         artistName: 'Stevie Wonder',
-        trackAudio: 'string'
+        trackAudio: 'https://p.scdn.co/mp3-preview/3b98fe75e40b1138281c30d35eb0783029b11976?cid=e0798695220f4b1296dc84f83a2c201c'
       },
       {
         trackName: 'Halo',
@@ -57,7 +57,58 @@ export const initialState = {
         artistName: 'David Bowie',
         trackAudio: 'https://p.scdn.co/mp3-preview/92caa5df4a00974b3065c2db0bb284dae528f62a?cid=e0798695220f4b1296dc84f83a2c201c'
       }],
-    wrongArtistNames: [['string', 'string', ..., 'string'], [...], ..., [...]]
+    wrongArtistNames: [
+      [
+        'Bloc Party',
+        'Interpol',
+        'Cage The Elephant'
+      ],
+      [
+        'Rick James',
+        'The Gap Band',
+        'Sly & The Family Stone'
+      ],
+      [
+        'Japan',
+        'Oingo Boingo',
+        'The Chameleons'
+      ],
+      [
+        'Shawn Mendes',
+        'Hailee Steinfeld',
+        'Lorde'
+      ],
+      [
+        'Heart',
+        'Crosby, Stills & Nash',
+        'The Cars'
+      ],
+      [
+        'R.E.M',
+        'Scorpions',
+        'Toto'
+      ],
+      [
+        'Roy Orbison',
+        'Jimi Hendrix',
+        'Wings'
+      ],
+      [
+        'Sly & The Family Stone',
+        'The Temptations',
+        'The Supremes'
+      ],
+      [
+        'The Pussycat Dolls',
+        'Fergie',
+        'Destiny\'s Child'
+      ],
+      [
+        'Brian Eno',
+        'The Velvet Underground',
+        'Joy Division'
+      ]
+    ]
   },
   user authentication: {
     userToken: 'string',
