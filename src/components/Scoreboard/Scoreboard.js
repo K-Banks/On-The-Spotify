9af14a18e7d385fc77bbae42f5fedbe273018637
Student <example@example.com>
2018-07-23 13:50:35 -0700

    Restructure scoreboard print out

--- src/components/Scoreboard/Scoreboard.js
+++ src/components/Scoreboard/Scoreboard.js
@@ -5,14 +5,22 @@ function Scoreboard(props) {
   return(
     <div>
       <h3>This is the scoreboard</h3>
-      {props.state.gameResults.map((results, key) =>
-        <ul key={key}>
-          <li>Artist: {results.artist}</li>
-          <li>Track: {results.track}</li>
-          <li>Correct: {results.answerString}</li>
-          <li>Points: {results.points}</li>
-        </ul>
-      )}
+      <table>
+        <tr>
+          <th>Artist Name</th>
+          <th>Track Name</th>
+          <th>Points</th>
+          <th>Answer</th>
+        </tr>
+        {props.state.gameResults.map((results, key) =>
+          <tr key={key}>
+            <td>{results.artist}</td>
+            <td>{results.track}</td>
+            <td>{results.points}</td>
+            <td>{results.answerString}</td>
+          </tr>
+        )}
+      </table>
     </div>
   );
 }
