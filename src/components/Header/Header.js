6ed204e4312b0e59e0afd648ced8eb00800c4f76
Kayl Eubanks <kayleubanks@gmail.com>
2018-07-25 16:19:01 -0700

    minor styling changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ function Header(){
       <header className="header">
         <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/168px-Spotify_logo_without_text.svg.png' className="logo" alt="logo" />
         <div className="bubble">
-          <h1>On The Spot-ify</h1>
+          <h1>ON THE SPOT-IFY</h1>
         </div>
       </header>
     </div>
