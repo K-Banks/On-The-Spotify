84346d09822d8b01e0f50d1b81c82474f11ca27a
Kayl Eubanks <kayleubanks@gmail.com>
2018-09-22 09:55:53 -0700

    Structure Token component, add button to print current URL to console

--- src/components/Token/Token.js
+++ src/components/Token/Token.js
@@ -1,29 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+function printLocation() {
+  let location = window.location.href;
+  console.log(location);
+}
+
 function Token(props) {
   return(
     <div className="bubbleSignIn">
-      <form onSubmit={login} className="formStyle">
-        <input
-          type='text'
-          id='token'
-          placeholder='Enter token string here'
-          ref={(input) => {_token = input;}}
-        />
-        <br></br>
-        <button type='submit'>Get my data!</button>
-      </form>
       <button type='button' onClick={() => {printLocation()}}>Print location</button>
     </div>
   )
 }
 
-SignIn.propTypes = {
-  gameStart: PropTypes.func,
-  scrapeUserData: PropTypes.func,
+Token.propTypes = {
   state: PropTypes.object,
   grabUserToken: PropTypes.func
 }
 
-export default SignIn;
+export default Token;
