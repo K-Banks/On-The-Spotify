a3047e85d148a8a77fdf95b6bc64327fe839d859
Kayl Eubanks <kayleubanks@gmail.com>
2018-10-01 15:01:43 -0700

    Refactor to use variable game length property in state

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -54,7 +54,7 @@ function Game(props){
         </div>
       </div>
     );
-  } else if (props.state.gameData.gameStatus===false && props.state.gameResults.length>=10) {
+  } else if (props.state.gameData.gameStatus===false && props.state.gameResults.length>=props.state.gameData.gameRounds) {
       return(
         <div className="spacer">
           <div className="game">
