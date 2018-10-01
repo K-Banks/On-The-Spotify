4ee26fd929ddc7eb9a56efb05f941c37e36a61b6
Kayl Eubanks <kayleubanks@gmail.com>
2018-10-01 13:01:25 -0700

    Updates

--- codeStorage/key/sample.js
+++
@@ -1,6 +0,0 @@
-export function getKey() {
-  const API_KEY = 'INSERT ACCESS TOKEN HERE';
-  return API_KEY;
-}
-
-// rename file to index.js
--- codeStorage/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import songReducer from './songReducer.js';
-
-const rootReducer = combineReducers({
-  songReducer: songReducer
-});
-
-export default rootReducer;
