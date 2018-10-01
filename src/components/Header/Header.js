55bdce487fddebadcca51b168b928d779e949b09
Kayl Eubanks <kayleubanks@gmail.com>
2018-10-01 10:36:53 -0700

    Add button to print state to console

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import './Header.css';
+import PropTypes from 'prop-types';
 
-function Header(){
+function Header(props){
+  function printState() {
+    console.log(props.state);
+  }
+
   return(
     <div>
       <header className="header">
         <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/168px-Spotify_logo_without_text.svg.png' className="logo" alt="logo" />
         <div className="bubble">
           <h1>ON THE SPOT-IFY</h1>
+          <button onClick={() => {printState()}}k>Print state</button>
         </div>
       </header>
     </div>
   )
+}
+
+Header.propTypes = {
+  state: PropTypes.object
 }
 
 export default Header;
