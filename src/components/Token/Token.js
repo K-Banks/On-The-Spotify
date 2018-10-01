14a7e99e1aba2308536d7b3694dbe46e0f54db03
Kayl Eubanks <kayleubanks@gmail.com>
2018-10-01 10:37:21 -0700

    Convert Token to class component, execute grabbing the token from url on componentDidMount

--- src/components/Token/Token.js
+++ src/components/Token/Token.js
@@ -1,36 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Token(props) {
-  function printLocation() {
-    let location = window.location.href;
-    console.log(location);
-    let startSlice = (location.indexOf("=") + 1);
-    console.log('start slice at ' + startSlice);
-    let endSlice = location.indexOf("&");
-    console.log('end slice at ' + endSlice);
-    let accessToken = location.slice(startSlice, endSlice);
-    console.log('access token is ' + accessToken);
-    props.grabUserToken(accessToken);
-    console.log(props.state);
-  }
+class Token extends React.Component {
 
-  function startGame(){
+  componentDidMount() {
+    console.log('hello this component did mount');
     let location = window.location.href;
     let sliceIndex = location.indexOf('access_token');
     console.log(sliceIndex);
     let baseURL = location.slice(0,sliceIndex);
     console.log('base url ' + baseURL);
+    let startSlice = (location.indexOf("=") + 1);
+    let endSlice = location.indexOf("&");
+    let accessToken = location.slice(startSlice, endSlice);
+    this.props.grabUserToken(accessToken);
     let redirectURL = baseURL + 'game';
     window.location = redirectURL;
   }
 
-  return(
-    <div className="bubbleSignIn">
-      <button type='button' onClick={() => {printLocation()}}>Print location</button>
-      <button type='button' onClick={() => {startGame()}}>Start</button>
-    </div>
-  )
+  render() {
+    return (
+      <div>
+        <p>This component shouldnt last long enough for you to see this.</p>
+      </div>
+    )
+  }
 }
 
 Token.propTypes = {
