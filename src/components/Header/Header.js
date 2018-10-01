d59eadece1f33603b4063bfa0014f1ff12a1f59d
Kayl Eubanks <kayleubanks@gmail.com>
2018-10-01 11:19:09 -0700

    added and removed button to log state to console

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import './Header.css';
-import PropTypes from 'prop-types';
 
-function Header(props){
-  function printState() {
-    console.log(props.state);
-  }
+function Header(){
 
   return(
     <div>
@@ -13,15 +9,10 @@ function Header(props){
         <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/168px-Spotify_logo_without_text.svg.png' className="logo" alt="logo" />
         <div className="bubble">
           <h1>ON THE SPOT-IFY</h1>
-          <button onClick={() => {printState()}}k>Print state</button>
         </div>
       </header>
     </div>
   )
-}
-
-Header.propTypes = {
-  state: PropTypes.object
 }
 
 export default Header;
