e3d1641504a03cdb12d742bf6ff9b2268ee16847
Kayl Eubanks <kayleubanks@gmail.com>
2018-10-01 12:49:40 -0700

    Add condition to inform user that data is being collected, used for slower API responses

--- src/components/RoundStart/RoundStart.js
+++ src/components/RoundStart/RoundStart.js
@@ -3,11 +3,19 @@ import PropTypes from 'prop-types';
 import './RoundStart.css';
 
 function RoundStart(props){
+  if (props.state.roundStatus) {
     return(
       <div className="roundStartStyle" onClick={()=>{props.toggleRoundStart();}}>
         <h1>READY TO START</h1>
       </div>
     );
+  } else {
+    return(
+      <div>
+        <h1>COLLECTING ROUND DATA</h1>
+      </div>
+    )
+  }
 }
 
 RoundStart.propTypes = {
