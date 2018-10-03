637843cefa38bb7f73fc3a6cf688a9370e3b798e
Kayl Eubanks <kayleubanks@gmail.com>
2018-10-03 13:42:17 -0700

    remove thank you

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,6 @@ function Header(){
         <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/168px-Spotify_logo_without_text.svg.png' className="logo" alt="logo" />
         <div className="bubble">
           <h1>ON THE SPOT-IFY</h1>
-          <h3>Thank you Carly!</h3>
         </div>
       </header>
     </div>
