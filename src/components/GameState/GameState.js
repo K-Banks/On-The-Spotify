1a4378335840b1de8f42245b66fe3d346bfd22df
Kayl Eubanks <kayleubanks@gmail.com>
2018-10-04 11:13:20 -0700

    Change redirect on restarting game

--- src/components/GameState/GameState.js
+++ src/components/GameState/GameState.js
@@ -4,7 +4,7 @@ import './GameState.css';
 
 function GameState(){
   function navigateHome() {
-    window.location = '/On-The-Spotify/#/';
+    window.location = 'https://k-banks.github.io/On-The-Spotify/#/';
   }
 
   return(
