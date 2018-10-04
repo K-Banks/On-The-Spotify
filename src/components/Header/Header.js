5c98435128388f78b7b699e0537b024355c91da5
Kayl Eubanks <kayleubanks@gmail.com>
2018-10-03 18:58:24 -0700

    Reformat header to include min width

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,9 +6,11 @@ function Header(){
   return(
     <div>
       <header className="header">
-        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/168px-Spotify_logo_without_text.svg.png' className="logo" alt="logo" />
-        <div className="bubble">
-          <h1>ON THE SPOT-IFY</h1>
+        <div className="minMaxStyle">
+          <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/168px-Spotify_logo_without_text.svg.png' className="logo" alt="logo" />
+          <div className="bubble">
+            <h1>ON THE SPOT-IFY</h1>
+          </div>
         </div>
       </header>
     </div>
