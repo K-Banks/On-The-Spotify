dba09c008e3e2e29e0b75cf66f92a14fb58fba18
Kayl Eubanks <kayleubanks@gmail.com>
2018-10-03 19:31:59 -0700

    finalize layout of gameState component

--- src/components/GameState/GameState.js
+++ src/components/GameState/GameState.js
@@ -3,9 +3,13 @@ import {Link} from 'react-router-dom';
 import './GameState.css';
 
 function GameState(){
+  function navigateHome() {
+    window.location = '/';
+  }
+
   return(
-    <div className="bubble2">
-      <Link className="linkStyle" to="/">RESTART GAME</Link>
+    <div className="bubble2" onClick={()=>navigateHome()}>
+      <h1>RESTART GAME</h1>
     </div>
   )
 }
